882df70829671ebc3a18e16e0f7218f5f7ce82d6
Firebottle <justin@justingarver.com>
2022-07-05 18:56:17 -0500

    feat: Get API working

--- src/utils/IGDB.ts
+++ src/utils/IGDB.ts
@@ -1,8 +1,5 @@
 import { twitchAccessToken, igdb, where, fields } from 'ts-igdb-client';
 
-// import config from '../../env-config';
-// const twitchConfig = JSON.parse(JSON.stringify(config.twitch));
-
 type TwitchSecrets = {
   client_id: string;
   client_secret: string;
@@ -16,17 +13,18 @@ const twitchSecrets: TwitchSecrets = {
       ? process.env.TWITCH_CLIENT_SECRET
       : '',
 };
-const accessToken = await twitchAccessToken(twitchSecrets);
-const client = igdb(twitchSecrets.client_id, accessToken);
 
 export async function getGameByName(
   name: string,
   requested_fields: '*' | [any, ...any[]] = ['name']
 ) {
+  const accessToken = await twitchAccessToken(twitchSecrets);
+  const client = igdb(twitchSecrets.client_id, accessToken);
   const { data } = await client
     .request('games')
     .pipe(fields(requested_fields), where('name', '~', name))
     .execute();
+  console.log(data);
 
   return data[0];
 }
