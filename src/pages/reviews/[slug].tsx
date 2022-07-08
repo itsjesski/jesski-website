ed64aa1c552ac0cad970118ef9fdbc386861b7c5
Firebottle <justin@justingarver.com>
2022-07-08 16:08:03 -0500

    fix: Fix screenshot issue when no screenshots provided.

--- src/pages/reviews/[slug].tsx
+++ src/pages/reviews/[slug].tsx
@@ -24,7 +24,7 @@ const DisplayPost = (props: FBReview) => (
         title={props.title}
         description={props.description}
         post={{
-          image: props.screenshots[0],
+          image: props.screenshots != null ? props.screenshots[0] : '',
           date: props.date,
           modified_date: props.modified_date,
         }}
