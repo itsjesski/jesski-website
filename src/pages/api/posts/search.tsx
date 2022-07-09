acfe7262b2b651030fc0091a703e75166a9fb958
Firebottle <justin@justingarver.com>
2022-07-09 02:22:25 -0500

    fix: Fix bugs where api endpoints were returning wrong format

--- src/pages/api/posts/search.tsx
+++ src/pages/api/posts/search.tsx
@@ -2,12 +2,17 @@ import { posts } from '../../../../public/cache/_posts';
 
 export default async function handler(
   req: { query: { s: string } },
-  res: { json: (results: any[]) => void }
+  res: { json: (results: any) => void }
 ) {
   // Currently only searching titles.
-  const results = req.query.s
+  const result = req.query.s
     ? posts.filter((post) => post.title.toLowerCase().includes(req.query.s))
     : [];
 
-  res.json(results);
+  const response = {
+    results: result,
+    totalPosts: posts.length,
+  };
+
+  res.json(response);
 }
