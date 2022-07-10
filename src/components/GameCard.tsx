3e7ecce4efbe385cdb47bc5d2baf4fbeee6bbc96
Firebottle <justin@justingarver.com>
2022-07-10 13:01:37 -0500

    feat: Add spinner to game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,9 +7,11 @@ import Link from 'next/link';
 import { IGDBGame } from '../pages/api/igdb/[title]';
 import { FBGame } from '../utils/Posts';
 import { GameScoreBox } from './GameScoreBox';
+import { Spinner } from './Spinner';
 
 const GameCard: React.FC<{ game: FBGame }> = ({ game }) => {
   const [fbGame, setGameData] = useState<IGDBGame>();
+  const [isLoading = true, setLoadingState] = useState<boolean>();
 
   useEffect(() => {
     if (fbGame != null) return;
@@ -19,19 +21,22 @@ const GameCard: React.FC<{ game: FBGame }> = ({ game }) => {
         .get(`/api/igdb/${game.title}`)
         .then((response: { data: IGDBGame }): void => {
           setGameData(response.data);
+          setLoadingState(false);
         })
         .catch(() => {});
     }
 
     getGameData();
-  }, [fbGame, game.title]);
+  }, [fbGame, game.title, setLoadingState]);
   return (
     <div className="p-2 xl:w-1/6 md:w-1/3 sm:w-1/2 w-full">
       <div className="group overflow-hidden relative max-w-xs shadow-steam">
         <Link href="/games/[slug]" as={`/games/${game.slug}`}>
           <a className="absolute z-10 top-0 bottom-0 left-0 right-0"></a>
         </Link>
-        <div>
+        <div className="w-full h-full flex items-center justify-center">
+          {isLoading && <Spinner></Spinner>}
+
           {fbGame?.cover && (
             <img
               src={fbGame?.cover}
