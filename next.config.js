cedd77d6cb8e20a9de8dc159981a90a7b8302cb9
Firebottle <justin@justingarver.com>
2022-07-15 13:54:39 -0500

    feat: Update icons

--- next.config.js
+++ next.config.js
@@ -31,6 +31,7 @@ module.exports = {
     return config;
   },
   env: {
+    SITE_URL: process.env.SITE_URL,
     TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
     TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET,
   },
