10906d7703ebf711f2694251c41d1e35582b11ca
Jesski <jess@jesski.com>
2024-08-18 12:29:04 -0500

    Testing to see if this fixes the api call issue.

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -2,7 +2,7 @@ export const AppConfig = {
   site_name: 'Jesski',
   title: 'Jesski',
   description: 'A site for all things Jesski.',
-  url: 'https://jesski.com',
+  url: '',
   locale: 'en',
   author: 'Jesski',
   pagination_size: 24,
