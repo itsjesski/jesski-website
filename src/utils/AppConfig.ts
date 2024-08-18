344f06cb166cd49030fd21b35e9d53ba855068e2
Jesski <jess@jesski.com>
2024-08-18 12:58:19 -0500

    Updating app config.

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -2,7 +2,7 @@ export const AppConfig = {
   site_name: 'Jesski',
   title: 'Jesski',
   description: 'A site for all things Jesski.',
-  url: '',
+  url: 'https://jesski.com',
   locale: 'en',
   author: 'Jesski',
   pagination_size: 24,
