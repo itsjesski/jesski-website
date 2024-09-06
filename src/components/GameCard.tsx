31be1d8433da4f3f03d41491f333eb51a5183ec0
Jesski <jess@jesski.com>
2024-09-06 17:38:37 -0500

    Placeholder image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -31,18 +31,22 @@ const GameCard: React.FC<{ game: FBGame; size: string }> = ({ game, size }) => {
         <Link
           href="/games/[slug]"
           as={`/games/${game.slug}`}
-          className="absolute z-10 top-0 bottom-0 left-0 right-0 text-cstyle-green"
+          className="absolute z-10 top-0 bottom-0 left-0 right-0 text-cstyle-highlight"
         ></Link>
         <div className="w-full h-full flex items-center justify-center">
           <img
-            src={`https://${game.cover}`}
+            src={`${
+              game.cover != null
+                ? `https://${game.cover}`
+                : '/assets/images/general/placeholder.png'
+            }`}
             alt="game image"
             className="block group-hover:opacity-40 transition-opacity duration-700 w-full"
           ></img>
         </div>
 
-        <div className="bottom-0 left-0 w-full bg-cstyle-background flex p-1 shadow-steam">
-          <div className="smallTitle text-cstyle-green text-sm overflow-hidden truncate font-semibold w-3/4">
+        <div className="bottom-0 left-0 w-full bg-white flex p-1 shadow-steam">
+          <div className="smallTitle text-cstyle-highlight text-sm overflow-hidden truncate font-semibold w-3/4">
             {game.title}
           </div>
           <GameScoreBox score={game.score}></GameScoreBox>
@@ -64,7 +68,7 @@ const GameCard: React.FC<{ game: FBGame; size: string }> = ({ game, size }) => {
     transition-all group-hover:top-1/3 group-hover:opacity-100"
         >
           <div className="py-4 text-xs px-7">
-            <div className="text-lg font-bold text-cstyle-green">
+            <div className="text-lg font-bold text-cstyle-highlight">
               {game?.title}
             </div>
             <div className="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
@@ -74,16 +78,6 @@ const GameCard: React.FC<{ game: FBGame; size: string }> = ({ game, size }) => {
               <span className="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
                 <span className="text-cstyle-text whitespace-nowrap text-xs md:text-sm">
                   {format(new Date(game.date), 'LLL d, yyyy')}
-                </span>
-              </span>
-            </div>
-            <div className="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
-              <span className="text-cstyle-text whitespace-nowrap text-xs md:text-sm">
-                Rating:{' '}
-              </span>
-              <span className="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
-                <span className="text-cstyle-text whitespace-nowrap text-xs md:text-sm">
-                  {game.score}%
                 </span>
               </span>
             </div>
