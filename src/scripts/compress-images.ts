584627fc0580beb65fb5c93bfee21f29f941b30e
Jesski <jess@jesski.com>
2025-03-19 13:02:12 -0500

    Trying image optimization

--- src/scripts/compress-images.ts
+++ src/scripts/compress-images.ts
@@ -1,30 +1,53 @@
+/* eslint-disable no-console */
 /* eslint-disable import/no-extraneous-dependencies */
-const imagemin = require('imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminSvgo = require('imagemin-svgo');
+import imagemin from 'imagemin';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
+import fs from 'fs';
+import path from 'path';
+
+const getFileSizeInBytes = (filePath: string): number => {
+  const stats = fs.statSync(filePath);
+  return stats.size;
+};
+
+const logFileSizes = (files: string[], message: string) => {
+  console.log(message);
+  files.forEach((file) => {
+    const filePath = path.resolve(file);
+    const fileSize = getFileSizeInBytes(filePath);
+    console.log(`File: ${filePath}, Size: ${fileSize} bytes`);
+  });
+};
 
 (async () => {
-  const files = await imagemin(
-    ['public/assets/images/art/**/*.{jpg,png,svg}'],
-    {
-      destination: 'public/assets/images/art/',
-      plugins: [
-        imageminMozjpeg({ quality: 80 }),
-        imageminPngquant({ quality: [0.6, 0.8] }),
-        imageminSvgo({
-          plugins: [
-            {
-              removeViewBox: false,
-            },
-          ],
-        }),
-      ],
-    }
-  );
+  const files = ['public/assets/images/art/**/*.{jpg,png,svg}'];
 
-  // eslint-disable-next-line no-console
-  console.log('Images optimized:', files);
+  // Log file sizes before compression
+  logFileSizes(files, 'Before compression:');
+
+  const optimizedFiles = await imagemin(files, {
+    destination: 'public/assets/images/art/',
+    plugins: [
+      imageminMozjpeg({ quality: 80 }),
+      imageminPngquant({ quality: [0.6, 0.8] }),
+      imageminSvgo({
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+        ],
+      }),
+    ],
+  });
+
+  // Log file sizes after compression
+  logFileSizes(
+    optimizedFiles.map((file) => file.destinationPath),
+    'After compression:'
+  );
 })();
 
 export {};
